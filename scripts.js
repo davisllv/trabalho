ec2ea751fdc6fffc728671d186ba314cf11ab876
Davi da Silva <davisllvsnt57@gmail.com>
2023-05-23 17:11:44 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,15 +5,16 @@ const body = document.querySelector("body");
 
 const modalContainer = document.querySelector(".modal__container");
 const textoCasamento = document.querySelector("#texto__casamento");
-
+const pressed = document.getElementById('pressed');
 let dataAtual = new Date();
-const novaData = dataAtual.setDate(dataAtual.getDate() + 30)
-const dia = dataAtual.getDate();
-const mes = dataAtual.getMonth();
-const ano = dataAtual.getFullYear();
-const dataFormatada = `${dia}/${mes}/${ano}`
+const novaData = new Date(dataAtual.setDate(dataAtual.getDate() + 30))
+console.log('novaData', novaData.getMonth())
+const dia = novaData.getDate();
+const mes = novaData.getMonth();
+const ano = novaData.getFullYear();
+const dataFormatada = `${dia}/${mes + 1}/${ano}`
 
-buttonNao.addEventListener('mouseover', (ev) => {
+buttonNao.addEventListener('mouseover', () => {
   const alturaAleatoria = Math.floor(Math.random() * ((body.clientHeight - 50) - 1 + 1) + 1);
   const larguraAleatoria = Math.floor(Math.random() * ((body.clientWidth - 60) - 1 + 1) + 1);
 
@@ -27,4 +28,9 @@ buttonSim.addEventListener('click', () => {
   buttonNao.style.display = 'none';
   modalContainer.style.display = 'flex';
   textoCasamento.textContent = `Nosso Casamento Está Marcado para o Dia ${dataFormatada}, esperarei você, vida!`
+})
+
+document.addEventListener('keydown', (event) => {
+  console.log('event', event)
+  if(event.key === 'Tab') event.preventDefault()
 })
